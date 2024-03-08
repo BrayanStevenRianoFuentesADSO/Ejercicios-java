be8ce562bb3d3f970fa77e5ae9610becd088ffc4
123Kevin4567 <kevinpolodr@gmail.com>
2024-03-08 16:19:37 -0500

    agregamos y editamos archivos

--- punto23.js
+++ punto23.js
@@ -2,3 +2,14 @@
 expresada en kilómetros por hora, proporcione la velocidad en metros por
 segundo.
 */
+
+
+/**divide el valor de velocidad entre 3,6 */
+
+
+
+let kmh = parseFloat(prompt("ingrese la velocidad en km/h"))
+
+let ms = kmh / 3.6
+
+document.write("la velocidad de su auto en m/s es de ", ms)
